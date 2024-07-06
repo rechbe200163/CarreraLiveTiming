a7d376b87d22f3623d54daab063a824a3ab82110
Dein Benutzername <rechbe200163@sr.htlweit.at>
2024-07-06 20:46:42 +0200

    refactor: Update Carrera Live Timing app to use socket.io for real-time updates

--- live-timing/app/page.tsx
+++ live-timing/app/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 
 import {
   Table,
@@ -9,8 +9,8 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import useWebSocket, { ReadyState } from "react-use-websocket";
 import React, { useState, useEffect } from "react";
+import io from "socket.io-client";
 
 interface RaceData {
   car_id: number;
@@ -22,39 +22,47 @@ interface RaceData {
 
 const CarreraData: React.FC = () => {
   const [raceData, setRaceData] = useState<RaceData[]>([]);
-  const { lastMessage, readyState } = useWebSocket("ws://localhost:8765");
 
   useEffect(() => {
-    if (lastMessage !== null) {
-      const newData: RaceData = JSON.parse(lastMessage.data);
-      console.log(newData);
+    const socket = io("http://localhost:5000", {
+      transports: ["websocket", "polling"],
+    });
+
+    socket.on("connect", () => {
+      console.log("WebSocket connection established.");
+      socket.emit("update");
+    });
+
+    socket.on("sum", (result: number) => {
+      console.log("Received sum:", result);
+    });
+
+    socket.on("update", (newData: any) => {
+      console.log("Received update:", newData);
       setRaceData((prevData) => {
-        // Update or add new data based on car_id or any unique identifier
         const existingIndex = prevData.findIndex(
           (data) => data.car_id === newData.car_id
         );
         if (existingIndex !== -1) {
-          // Update existing data
           prevData[existingIndex] = newData;
           return [...prevData];
         } else {
-          // Add new data
           return [...prevData, newData];
         }
       });
-    }
-  }, [lastMessage]);
+    });
 
-  useEffect(() => {
-    if (readyState === ReadyState.OPEN) {
-      console.log("WebSocket connection established.");
-    } else if (readyState === ReadyState.CLOSED) {
+    socket.on("disconnect", () => {
       console.log("WebSocket connection closed.");
-    }
-  }, [readyState]);
-  
-  return(
-  <div className="flex flex-col items-center font-extrabold text-3xl gap-10">
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  return (
+    <div className="flex flex-col items-center font-extrabold text-3xl gap-10">
       <div>
         <h1>Live Daten</h1>
       </div>
@@ -72,7 +80,7 @@ const CarreraData: React.FC = () => {
           </TableHeader>
           <TableBody>
             {raceData.map((car) => (
-              <TableRow key={ raceData.indexOf(car) }>
+              <TableRow key={car.car_id}>
                 <TableCell className="font-medium">{car.car_id}</TableCell>
                 <TableCell>{car.position}</TableCell>
                 <TableCell>{car.last_lap_time}</TableCell>
