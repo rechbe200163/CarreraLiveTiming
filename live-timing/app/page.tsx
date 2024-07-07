23d454a641c03a2dea688a95a129ddd3db86e384
Michael Rechberger <rechbe200163@sr.htlweiz.at>
2024-07-07 17:06:10 +0200

    refactor: Update Carrera Live Timing app to include position field in race data

--- live-timing/app/page.tsx
+++ live-timing/app/page.tsx
@@ -13,21 +13,18 @@ import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
 
 interface RaceData {
-  car_id: number; // Reflecting the change from 'num'
-  time: number;
+  car_id: number;
+  position: number;
   last_lap_time: number;
   best_lap_time: number;
   laps: number;
-  pits: number;
-  fuel: number;
-  pit: boolean;
 }
 
 const CarreraData: React.FC = () => {
   const [raceData, setRaceData] = useState<RaceData[]>([]);
 
   useEffect(() => {
-    const socket = io("http://localhost:5000", {
+    const socket = io("http://localhost:8765", {
       transports: ["websocket", "polling"],
     });
 
@@ -38,20 +35,7 @@ const CarreraData: React.FC = () => {
 
     socket.on("update", (newData: RaceData[]) => {
       console.log("Received update:", newData);
-      setRaceData((prevData) => {
-        const updatedData = [...prevData];
-        newData.forEach((newCarData) => {
-          const existingIndex = updatedData.findIndex(
-            (data) => data.car_id === newCarData.car_id
-          );
-          if (existingIndex !== -1) {
-            updatedData[existingIndex] = newCarData;
-          } else {
-            updatedData.push(newCarData);
-          }
-        });
-        return updatedData;
-      });
+      setRaceData(newData);
       console.log("Race data updated", raceData);
     });
 
@@ -75,26 +59,20 @@ const CarreraData: React.FC = () => {
           <TableHeader>
             <TableRow>
               <TableHead className="w-[100px]">Car</TableHead>
-              <TableHead className="w-[100px]">Time</TableHead>
+              <TableHead className="w-[100px]">Position</TableHead>
               <TableHead className="w-[100px]">Last Lap</TableHead>
               <TableHead className="w-[100px]">Best Lap</TableHead>
               <TableHead className="w-[100px]">Laps</TableHead>
-              <TableHead className="w-[100px]">Pits</TableHead>
-              <TableHead className="w-[100px]">Fuel</TableHead>
-              <TableHead className="w-[100px]">In Pit</TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
             {raceData.map((car) => (
               <TableRow key={car.car_id}>
                 <TableCell className="font-medium">{car.car_id}</TableCell>
-                <TableCell>{car.time}</TableCell>
+                <TableCell>{car.position}</TableCell>
                 <TableCell>{car.last_lap_time}</TableCell>
                 <TableCell>{car.best_lap_time}</TableCell>
                 <TableCell>{car.laps}</TableCell>
-                <TableCell>{car.pits}</TableCell>
-                <TableCell>{car.fuel}</TableCell>
-                <TableCell>{car.pit ? "Yes" : "No"}</TableCell>
               </TableRow>
             ))}
           </TableBody>
--- live-timing/lib/utils.ts
+++ live-timing/lib/utils.ts
@@ -1,6 +1,14 @@
 import { type ClassValue, clsx } from "clsx"
+import { format } from "path"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+// Format time function provided in milliseconds
+export function formatTime(time: number) {
+  const minutes = Math.floor(time / 60000)
+  const seconds = ((time % 60000) / 1000).toFixed(3)
+  return `${minutes}:${parseInt(seconds) < 10 ? "0" : ""}${seconds}`
 }
