587d8949279b36627b5a1568d3d83ed8dd54f5f7
Michael Rechberger <71632874+rechbe200163@users.noreply.github.com>
2024-07-07 10:58:35 +0200

    refactor: Update Carrera Live Timing app to include additional race data fields

--- live-timing/app/page.tsx
+++ live-timing/app/page.tsx
@@ -13,11 +13,14 @@ import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
 
 interface RaceData {
-  car_id: number;
-  position: number;
+  car_id: number; // Reflecting the change from 'num'
+  time: number;
   last_lap_time: number;
   best_lap_time: number;
   laps: number;
+  pits: number;
+  fuel: number;
+  pit: boolean;
 }
 
 const CarreraData: React.FC = () => {
@@ -31,22 +34,25 @@ const CarreraData: React.FC = () => {
     socket.on("connect", () => {
       console.log("WebSocket connection established.");
       socket.emit("update");
-      console.log("Update event send");
     });
 
-    socket.on("update", (newData: RaceData) => {
+    socket.on("update", (newData: RaceData[]) => {
       console.log("Received update:", newData);
       setRaceData((prevData) => {
-        const existingIndex = prevData.findIndex(
-          (data) => data.car_id === newData.car_id
-        );
-        if (existingIndex !== -1) {
-          prevData[existingIndex] = newData;
-          return [...prevData];
-        } else {
-          return [...prevData, newData];
-        }
+        const updatedData = [...prevData];
+        newData.forEach((newCarData) => {
+          const existingIndex = updatedData.findIndex(
+            (data) => data.car_id === newCarData.car_id
+          );
+          if (existingIndex !== -1) {
+            updatedData[existingIndex] = newCarData;
+          } else {
+            updatedData.push(newCarData);
+          }
+        });
+        return updatedData;
       });
+      console.log("Race data updated", raceData);
     });
 
     socket.on("disconnect", () => {
@@ -69,20 +75,26 @@ const CarreraData: React.FC = () => {
           <TableHeader>
             <TableRow>
               <TableHead className="w-[100px]">Car</TableHead>
-              <TableHead className="w-[100px]">Position</TableHead>
+              <TableHead className="w-[100px]">Time</TableHead>
               <TableHead className="w-[100px]">Last Lap</TableHead>
               <TableHead className="w-[100px]">Best Lap</TableHead>
               <TableHead className="w-[100px]">Laps</TableHead>
+              <TableHead className="w-[100px]">Pits</TableHead>
+              <TableHead className="w-[100px]">Fuel</TableHead>
+              <TableHead className="w-[100px]">In Pit</TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
             {raceData.map((car) => (
               <TableRow key={car.car_id}>
                 <TableCell className="font-medium">{car.car_id}</TableCell>
-                <TableCell>{car.position}</TableCell>
+                <TableCell>{car.time}</TableCell>
                 <TableCell>{car.last_lap_time}</TableCell>
                 <TableCell>{car.best_lap_time}</TableCell>
                 <TableCell>{car.laps}</TableCell>
+                <TableCell>{car.pits}</TableCell>
+                <TableCell>{car.fuel}</TableCell>
+                <TableCell>{car.pit ? "Yes" : "No"}</TableCell>
               </TableRow>
             ))}
           </TableBody>
