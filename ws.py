92003adb0194d63f9bcaa2f67d3eedf740c4c3fe
Michael Rechberger <rechbe200163@sr.htlweiz.at>
2024-07-08 14:04:35 +0200

    feat: Add max_laps parameter to RaceSimulation constructor

--- ws.py
+++ ws.py
@@ -7,7 +7,7 @@ import socketio
 eventlet.monkey_patch()
 
 # Define mock sio
-sio = socketio.Server(cors_allowed_origins='*')
+sio = socketio.Server(cors_allowed_origins='http://localhost:3000')
 app = socketio.WSGIApp(sio)
 
 
@@ -25,13 +25,14 @@ class Driver:
 
 
 class RaceSimulation:
-    def __init__(self, num_drivers):
+    def __init__(self, num_drivers, max_laps):
         self.drivers = [Driver(i) for i in range(num_drivers)]
         self.running = False
+        self.max_laps = max_laps
 
     def update(self, blink=lambda: (time.time() * 2) % 2 == 0):
         drivers = [driver.__dict__ for driver in self.drivers if driver.time]
-        drivers.sort(key=lambda driver: driver['laps'])
+        drivers.sort(key=lambda driver: driver['bestlap'])
         return drivers
 
     def run(self):
@@ -39,8 +40,9 @@ class RaceSimulation:
         while self.running:
             for driver in self.drivers:
                 driver.time = int(time.time() * 1000)
-                driver.laptime = int(random.uniform(7.4, 11.0) * 1000)
-                driver.bestlap = min(driver.bestlap or driver.laptime, driver.laptime)
+                driver.laptime = int(random.uniform(7.4, 20.0) * 1000)
+                driver.bestlap = min(
+                    driver.bestlap or driver.laptime, driver.laptime)
                 driver.laps += 1
                 driver.pits = random.randint(0, 5)
                 driver.fuel = False
@@ -51,11 +53,14 @@ class RaceSimulation:
             eventlet.sleep()
             eventlet.sleep(random.uniform(7.4, 11.0))
 
+            if all(driver.laps >= self.max_laps for driver in self.drivers):
+                self.stop()
+
     def stop(self):
         self.running = False
 
 
-simulation = RaceSimulation(num_drivers=5)
+simulation = RaceSimulation(num_drivers=5, max_laps=20)
 
 
 @sio.event
@@ -68,11 +73,23 @@ def disconnect(sid):
     print('Client disconnected:', sid)
 
 
-def start_simulation():
+@sio.event
+def qualifing():
+    ...
+
+
+@sio.event
+def stop(sid):
+    simulation.stop()
+    sio.emit("stop_success", "stopped session successfully", to=sid)
+
+
+@sio.event
+def start(sid):
     eventlet.spawn(simulation.run)
+    sio.emit("start_success", "started session successfully", to=sid)
 
 
 if __name__ == '__main__':
-    start_simulation()
     eventlet.wsgi.server(eventlet.listen(('', 8765)), app)
     simulation.stop()
