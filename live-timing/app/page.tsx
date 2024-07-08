dd182660d043c5ca5910747aec22593384c68dcb
Michael Rechberger <rechbe200163@sr.htlweiz.at>
2024-07-08 12:04:28 +0200

    refactor: Update Carrera Live Timing app to include header in race data table

--- live-timing/app/page.tsx
+++ live-timing/app/page.tsx
@@ -1,10 +1,14 @@
-"use client";
-
 import TableComponent from "@/components/dashboard/tableComponent";
 
 const CarreraData: React.FC = () => {
-
-  return <TableComponent />;
+  return (
+    <div className="flex flex-col items-center font-extrabold text-3xl gap-10">
+      <div className="flex flex-row-reverse p-5">
+        <h1>Live Daten</h1>
+      </div>
+      <TableComponent />
+    </div>
+  );
 };
 
 export default CarreraData;
