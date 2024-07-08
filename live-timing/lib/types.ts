8d142988cc01d6c69022e2c08ffd887ef4620f6d
Michael Rechberger <rechbe200163@sr.htlweiz.at>
2024-07-08 10:26:11 +0200

    refactor: Update Carrera Live Timing app to include type definitions for race data and utility functions

---
+++ live-timing/lib/types.ts
@@ -0,0 +1,12 @@
+export interface RaceData {
+  num: number;
+  position: number;
+  time: number;
+  laptime: number;
+  bestlap: number;
+  laps: number;
+  pits: number;
+  fuel: boolean;
+  pit: boolean;
+  has_fastest_lap: boolean;
+}
--- live-timing/lib/utils.ts
+++ live-timing/lib/utils.ts
@@ -1,14 +1,37 @@
-import { type ClassValue, clsx } from "clsx"
-import { format } from "path"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { format } from "path";
+import { twMerge } from "tailwind-merge";
+import { RaceData } from "./types";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
 // Format time function provided in milliseconds
-export function formatTime(time: number) {
-  const minutes = Math.floor(time / 60000)
-  const seconds = ((time % 60000) / 1000).toFixed(3)
-  return `${minutes}:${parseInt(seconds) < 10 ? "0" : ""}${seconds}`
+export function formatTime(time: number, type: string = "racetime") {
+  if (type === "laptime") {
+    return formatLapTime(time);
+  }
+  if (type === "racetime") {
+    return formatRaceTime(time);
+  }
+}
+
+function formatRaceTime(time: number) {
+  const seconds = ((time % 60000) / 1000).toFixed(3);
+  return `${parseInt(seconds) < 10 ? "0" : ""}${seconds}`;
+}
+
+function formatLapTime(time: number) {
+  const minutes = Math.floor(time / 60000);
+  const seconds = ((time % 60000) / 1000).toFixed(3);
+  return `${parseInt(seconds) < 10 ? "0" : ""}${seconds}`;
+}
+
+export function getFastestLapCars(raceData: RaceData[]) {
+  const fastestLapTime = Math.min(...raceData.map((data) => data.bestlap));
+  return raceData.map((data) => ({
+    ...data,
+    has_fastest_lap: data.bestlap === fastestLapTime,
+  }));
 }
