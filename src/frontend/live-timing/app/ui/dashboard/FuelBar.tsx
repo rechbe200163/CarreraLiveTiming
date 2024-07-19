0d562c7ea8b6665f2a840177bc2389c161670c12
Michael Rechberger <71632874+rechbe200163@users.noreply.github.com>
2024-07-19 10:09:45 +0200

    new background color for FuelBar

--- src/frontend/live-timing/app/ui/dashboard/FuelBar.tsx
+++ src/frontend/live-timing/app/ui/dashboard/FuelBar.tsx
@@ -7,7 +7,7 @@ type FuelStatusBarProps = {
 
 export default function FuelStatusBar({ fuelLevel }: FuelStatusBarProps) {
   return (
-    <div className="flex flex-row items-center justify-center px-2 -skew-x-12 ">
+    <div className="flex flex-row items-center justify-center px-2 -skew-x-6 ">
       <div className="w-full bg-gray-500/40 h-4  rounded-md overflow-hidden my-6">
         <div
           className={cn("h-full items-center justify-center text-white", {
